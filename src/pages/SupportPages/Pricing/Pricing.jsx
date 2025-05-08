7db8b37351b3fe3b40a82d926731464493190395
Parthi6 <parthi0815@gmail.com>
2025-05-09 01:38:02 +0530

    payment popup mobile response update

--- src/pages/SupportPages/Pricing/Pricing.jsx
+++ src/pages/SupportPages/Pricing/Pricing.jsx
@@ -3,22 +3,15 @@ import SupportPagesLayout from "../../../layouts/SupportPagesLayout";
 import ActionBox from "../../../components/ActionBox/ActionBox";
 import styles from "../SupportPages.module.css";
 import Price from "../../../components/Price_Popup/Price";
-import {readUserField} from "../../../services/api";
 
 const Pricing = () => {
   return (
     <SupportPagesLayout>
       <ActionBox>
-        <div className={`${styles.supportContent} customScroll`}>
+        <div className={`${styles.supportContent} ${styles.pricingPage}`}>
           <h1 className={styles.pageTitle}>Pricing</h1>
-          <p className={styles.description}>
-            Explore our pricing plans to find the perfect fit for your needs.
-            Each interview slot is 15 minutes long, covering the breadth of the
-            Chevening interview.
-          </p>
-          <div className={styles.pricingContainer}>
-           <Price/>
-          </div>
+          
+          <Price showContainerBox={true} />
         </div>
       </ActionBox>
     </SupportPagesLayout>
